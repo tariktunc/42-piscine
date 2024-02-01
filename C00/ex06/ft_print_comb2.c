a313c5e67e107fbfb431df3876b9928b7378b62b
TARIK TUNC <tarik.tunc@ogr.iu.edu.tr>
2024-02-01 19:41:25 +0300

    Update ft_print_comb2.c

--- C00/ex06/ft_print_comb2.c
+++ C00/ex06/ft_print_comb2.c
@@ -1,55 +1,56 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_print_comb.c                                    :+:      :+:    :+:   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ttunc <ttunc@student.42istanbul.com.tr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/27 22:53:45 by ttunc             #+#    #+#             */
-/*   Updated: 2024/02/01 17:53:23 by ttunc            ###   ########.fr       */
+/*   Created: 2024/01/30 20:10:03 by ttunc             #+#    #+#             */
+/*   Updated: 2024/02/01 17:52:49 by ttunc            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	ft_putchar(char a, char b, char c)
+void	ft_putchar(int a, int b)
 {
-	write(1, &a, 1);
-	write(1, &b, 1);
+	char	c;
+
+	c = '0' + (a / 10);
 	write(1, &c, 1);
-	if (a != 7 && b != 8 && c != 9)
-	{
+	c = '0' + (a % 10);
+	write(1, &c, 1);
+	write(1, " ", 1);
+	c = '0' + (b / 10);
+	write(1, &c, 1);
+	c = '0' + (b % 10);
+	write(1, &c, 1);
+	if (a != 98 && a != 99)
 		write(1, ",", 1);
-		write(1, " ", 1);
-	}
+	write(1, " ", 1);
 }
 
-void	ft_print_comb(void)
+void	ft_print_comb2(void)
 {
 	int	a;
 	int	b;
-	int	c;
 
-	a = '0';
-	while (a <= '7')
+	a = 0;
+	b = 1;
+	while (a <= 98)
 	{
-		b = a + 1;
-		while (b <= '8')
+		while (b <= 99)
 		{
-			c = b + 1;
-			while (c <= '9')
-			{
-				ft_putchar(a, b, c);
-				c++;
-			}
+			ft_putchar(a, b);
 			b++;
 		}
 		a++;
+		b = a + 1;
 	}
 }
 
 int	main(void)
 {
-	ft_print_comb();
+	ft_print_comb2();
 	return (0);
 }
