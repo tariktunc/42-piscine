ad5ee65aa286128794c44c7ce93167b73299dd9a
TARIK TUNC <tarik.tunc@ogr.iu.edu.tr>
2024-02-01 19:41:12 +0300

    Update ft_print_comb2.c

--- C00/ex06/ft_print_comb2.c
+++ C00/ex06/ft_print_comb2.c
@@ -1,41 +1,55 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
+/*   ft_print_comb.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ttunc <ttunc@student.42istanbul.com.tr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/30 20:10:03 by ttunc             #+#    #+#             */
-/*   Updated: 2024/01/30 22:06:06 by ttunc            ###   ########.fr       */
+/*   Created: 2024/01/27 22:53:45 by ttunc             #+#    #+#             */
+/*   Updated: 2024/02/01 17:53:23 by ttunc            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	ft_putchar(char c)
+void	ft_putchar(char a, char b, char c)
 {
+	write(1, &a, 1);
+	write(1, &b, 1);
 	write(1, &c, 1);
+	if (a != 7 && b != 8 && c != 9)
+	{
+		write(1, ",", 1);
+		write(1, " ", 1);
+	}
 }
 
-void	ft_print_bom2(void)
+void	ft_print_comb(void)
 {
 	int	a;
 	int	b;
+	int	c;
 
-	a = 0;
-	while (a <= '98')
+	a = '0';
+	while (a <= '7')
 	{
-		b = a + 0;
-		while (b <= '99')
+		b = a + 1;
+		while (b <= '8')
 		{
-			write(1, &a, 1);
-			write(1, &b, 1);
-			if(a != '98' || b != '99')
-				write(1, ", ", 1);
+			c = b + 1;
+			while (c <= '9')
+			{
+				ft_putchar(a, b, c);
+				c++;
+			}
 			b++;
 		}
 		a++;
 	}
 }
 
-// Wait...
+int	main(void)
+{
+	ft_print_comb();
+	return (0);
+}
