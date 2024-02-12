3347a6af4814b85c4d8c1879c6f556026820e96c
TARIK TUNC <tarik.tunc@ogr.iu.edu.tr>
2024-02-12 13:37:38 +0300

    Add files via upload

---
+++ C02/ex09/ft_strcapitalize.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcapitalize.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ttunc <ttunc@student.42istanbul.com.tr>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/11 01:25:20 by ttunc             #+#    #+#             */
+/*   Updated: 2024/02/11 05:29:51 by ttunc            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strlowcase(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'A' && str[i] <= 'Z')
+			str[i] += 32;
+		i++;
+	}
+	return (str);
+}
+
+char	*ft_strcapitalize(char *str)
+{
+	int		i;
+	int		check;
+
+	i = 0;
+	check = 1;
+	ft_strlowcase(str);
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			if (check == 1)
+				str[i] -= 32;
+			check = 0;
+		}
+		else if (str[i] >= '0' && str[i] <= '9')
+			check = 0;
+		else
+			check = 1;
+		i++;
+	}
+	return (str);
+}
