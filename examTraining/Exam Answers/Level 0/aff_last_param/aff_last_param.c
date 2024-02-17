6819a84bcb6a50bd761d9e50f3964a1630184341
tariktunc <tarktunc@hotmail.com>
2024-02-18 00:20:04 +0200

    new exam v01 and exam answers

---
+++ examTraining/Exam Answers/Level 0/aff_last_param/aff_last_param.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+
+int		main(int argc, char *argv[])
+{
+	int	i = -1;
+
+	if (argc > 1)
+	{
+		while (argv[argc - 1][++i])
+		{
+			write(1, &argv[argc - 1][i], 1);
+		}
+	}
+	write(1, "\n", 1);
+	return (0);
+}
